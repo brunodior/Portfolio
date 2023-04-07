555bf96e9ecbc74bada4fe8aefb03beb81b7ac1b
brunnoaraujo0 <helio.barros.708@ufrn.edu.br>
2023-04-07 09:00:32 -0300

    80%

--- blog/script.js
+++ blog/script.js
@@ -15,3 +15,42 @@ document.querySelector('.material-symbols-outlined').addEventListener('click', (
 
 
 })
+
+window.sr = ScrollReveal({reset: true});
+
+ScrollReveal().reveal('.foto', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.logo', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+
+ScrollReveal().reveal('.post-item', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.doPost', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
