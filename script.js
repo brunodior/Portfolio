0ce3f7dddc5eb9579a8dc7f685ab685fe512ed09
brunnoaraujo0 <helio.barros.708@ufrn.edu.br>
2023-04-07 15:01:15 -0300

    ta pertooo

--- script.js
+++ script.js
@@ -63,7 +63,6 @@ window.addEventListener('scroll', () => {
 
 })
 
-
 function monitorar(){
 
         if(window.scrollY > 100){
@@ -75,3 +74,94 @@ function monitorar(){
         }
     }
     ;
+
+    window.sr = ScrollReveal({reset: true});
+
+ScrollReveal().reveal('.inicio-informacoes', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.inicio-img', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.video', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.include-texto', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.sobreMim-texto', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:100,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.sobreMim-img', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+ScrollReveal().reveal('.hobbies-item', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+ScrollReveal().reveal('.portifolio-item', {
+    delay: 300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0
+    }
+})
+
+const menuItens = document.querySelectorAll('.navegacao a[href^="#"]'); //pega todos a
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick); //coloca um evento de click para cada a
+})
+
+function scrollToIdOnClick(event) { //esse event e passado cada a cada vez que e clicado, addeventlistener
+    event.preventDefault();
+    const element = event.target; // o event vem com varios atributos, o target pega so a tag "a"
+    const id = element.getAttribute('href'); //pega so atributo href dentro do element
+    const to = document.querySelector(id).offsetTop; //esse offsettop pega o top da section
+
+    window.scroll({
+        top: to - 100,
+        behavior: "smooth",
+    })
+}
